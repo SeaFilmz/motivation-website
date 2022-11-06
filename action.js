57990c95f103f228e739b8ce31cddab12ae2d21d
Daniel Schmidt <danielschmidt18@gmail.com>
2022-11-05 19:15:53 -0700

    Reordering Location of Functions

--- action.js
+++ action.js
@@ -1,3 +1,19 @@
+const issueButton = document.querySelector("#issueButton");
+const issueRhyme = `If you have an issue you might need a tissue.`;
+clickEvent(issueButton, "#issueText", issueRhyme, "issue");
+
+const longButton = document.querySelector("#longButton");
+const ongRhyme = `If you think long you might think wrong.`;
+clickEvent(longButton, "#longText", ongRhyme, "ong");
+
+const schoolButton = document.querySelector("#schoolButton");
+const oolRhyme = `School is cool.`;
+clickEvent(schoolButton, "#schoolText", oolRhyme, "ool");
+
+const hateButton = document.querySelector("#hateButton");
+const ateRhyme = `Don't Hate Appreciate`;
+clickEvent(hateButton, "#hateText", ateRhyme, "ate");
+
 function wordCount(wordArr, suffix) {
   return wordArr.reduce((count, word) => {
     if (word.replaceAll(/[\.\?\!]/g, "").endsWith(suffix)) {
@@ -18,19 +34,3 @@ function clickEvent(button, selector, rhyme, suffix) {
     document.querySelector(selector).innerHTML = displayText;
   });
 }
-
-const issueButton = document.querySelector("#issueButton");
-const issueRhyme = `If you have an issue you might need a tissue.`;
-clickEvent(issueButton, "#issueText", issueRhyme, "issue");
-
-const longButton = document.querySelector("#longButton");
-const ongRhyme = `If you think long you might think wrong.`;
-clickEvent(longButton, "#longText", ongRhyme, "ong");
-
-const schoolButton = document.querySelector("#schoolButton");
-const oolRhyme = `School is cool.`;
-clickEvent(schoolButton, "#schoolText", oolRhyme, "ool");
-
-const hateButton = document.querySelector("#hateButton");
-const ateRhyme = `Don't Hate Appreciate`;
-clickEvent(hateButton, "#hateText", ateRhyme, "ate");
